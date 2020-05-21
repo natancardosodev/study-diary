76074b56046e982befcf4d9d0c6016f3f95d75f3
Natan Cardoso <natancb7@gmail.com>
2020-05-21 19:09:00 -0300

    ADD - Configuração do Firebase

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.3.
 
+## Firebase
+Deve-se criar um projeto e um realtime databse, criar um app para web e capturar as configurações.
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAXN6FbDv2j-8GiSWlE1IhhWCImRBpalGg",
+    authDomain: "study-diary-327ea.firebaseapp.com",
+    databaseURL: "https://study-diary-327ea.firebaseio.com",
+    projectId: "study-diary-327ea",
+    storageBucket: "study-diary-327ea.appspot.com",
+    messagingSenderId: "1040512592582",
+    appId: "1:1040512592582:web:6dd2e73235b23f7e0d05c7",
+    measurementId: "G-QHFZSWB2W0"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
