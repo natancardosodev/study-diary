250b608fc95b74b96df0f45c2b0b4dcd0a1cd034
Natan Cardoso <natancb7@gmail.com>
2020-05-21 22:39:54 -0300

    ADD - Configurando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ListComponent } from './studies/list/list.component';
+import { EditComponent } from './studies/edit/edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'edit', component: EditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
